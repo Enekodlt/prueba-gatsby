585f0db22c3bd4ebf3dd016a2bbf3c3f48f12611
Eneko <enekodlt@gmail.com>
2020-08-03 16:36:20 +0200

    Cambiar la fuente de los posts

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://enekodelatorre.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://eneko-ghost.herokuapp.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
